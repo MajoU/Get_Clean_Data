4c82cdb3ed930db76dac8e4a593d78916d54dd15
MajoU <majo2906@gmail.com>
2014-08-30 17:55:06 +0200

    final modification of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # loading usefull packages
 library(plyr)
-library(reshape2)
+library(data.table)
 
 # set working directory
 setwd("~/Data_Science/Getting_and_Cleaning_Data/Get_and_Clean_Data/UCI_HAR_Dataset/")
@@ -14,43 +14,43 @@ train_df <- do.call("cbind", lapply(grep("train", files[c(2,6,4)], value = T), r
 # read test files in specific order from files and convert it to data frame
 test_df <- do.call("cbind", lapply(grep("test", files[c(1,5,3)], value = T), read.table))
 
-# merging two data frame - train_df and test_df
+# join two data frame - train_df and test_df
 train_test <- data.table(rbind(train_df, test_df))
 
 # read features.txt and create column names 
 features <- setnames(fread("./features.txt"), c("V1", "V2"), c("id", "label"))
-# data frame for cleaning features labels 
+# data frame for cleaning features label
 clean <- data.frame(old = c("[()-,]","std","BodyBody","mean"), new = c("","Std", "Body","Mean"))
 # clean the features labels through lapply with gsub conditions - change
 # old variables for new variables
 g <- lapply(1:4, function(x) features[, label := gsub(clean$old[x], clean$new[x], label)])
-# change merge_df colnames as sucject, activity and features label names
+# change train_test colnames as sucject, activity and features label names
 setnames(train_test, c("subject", "activity", features$label))
 
 # Extract the relevant features name from the features label
 mean_std <- grep("Mean|Std", features$label, value=T)
-# subset merge_df by specific column and names from mean_std
-sub_train_test <- merge_df[, c("subject", "activity", mean_std), with = F]
+# subset train_test by specific column and names from mean_std
+sub_train_test <- train_test[, c("subject", "activity", mean_std), with = F]
 
 #--------------------------------------------------------------------
 #  ALTERNATIVE 
 
 # --- data table ------------------------------------------- 
-# sub_train_test <- subset(merge_df, select = c("subject", "activity", mean_std))
+# sub_train_test <- subset(train_test, select = c("subject", "activity", mean_std))
 
 # --- data frame -------------------------------------------
-# sub_train_test <- merge_df[, c("subject", "activity", mean_std_names)]
+# sub_train_test <- train_test[, c("subject", "activity", mean_std_names)]
 
 #-------------------------------------------------------------------
 
 # read activity_labels.txt and create new column names
 activ <- setnames(fread("./activity_labels.txt"), c("V1", "V2"), c("activity", "label"))
 
-# merge sub_train_test and activ DT by activity id. This allocates
+# merge sub_train_test and activ DT by activity. This allocates
 # activity id to activity labels in new column 'label'
 tidy_df <- merge(setkey(activ), setkey(sub_train_test), by = "activity")
 
-# relocate data set columns and order activity_id variables by subject for better overview 
+# reorder tidy_df columns by specific order
 setcolorder(tidy_df, c("subject", "label", "activity", mean_std))
 # arrange tidy_df by subject
 tidy_df <- arrange(tidy_df, subject)
